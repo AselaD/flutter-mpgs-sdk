2432a13a026fa10012bad6a91d015be254241e6f
Deeptha Senanayake <chalithadeeptha@gmail.com>
2019-12-05 17:39:18 +0530

    removed unnecessary header files

--- ios/Classes/FlutterMPGSPlugin.m
+++ ios/Classes/FlutterMPGSPlugin.m
@@ -1,5 +1,4 @@
 #import "FlutterMPGSPlugin.h"
-#import <flutter_mpgs/flutter_mpgs-Swift.h>
 
 @implementation FlutterMPGSPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
